e596c961db8991f38f4c30119cb98d4510818496
wobkobi <imthatguyh9@gmail.com>
2023-09-30 23:26:08 +1300

    reformatting files + requirements.txt added

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-YOUR_ID = int(os.getenv("YOUR_ID"))  # User ID
+YOUR_ID = int(os.getenv("YOUR_ID"))
 
-REGEX_NWORD_HARDR = re.compile(r"\b(n*[1i!l]+[g9]{2,}[3e]+[r5]+s*)", re.IGNORECASE)
+REGEX_NWORD_HARDR = re.compile(
+    r"\b(n*[1i!l]+[g9]{2,}[3e]+[r5]+s*)", re.IGNORECASE)
 REGEX_NWORD = re.compile(r"\b(n*[1i!l]+[g9]{2,}[a4]+s*)", re.IGNORECASE)
 DRAMA_LLAMA = re.compile(r"(?:l+|d+r+)a+m+a", re.IGNORECASE)
 NWORD = "🇳 🇼 🇴 🇷 🇩"
@@ -21,22 +22,22 @@ GIRLS = re.compile(
     re.IGNORECASE,
 )
 
+bot = commands.Bot(command_prefix="{}", intents=discord.Intents.all())
 
-# Load data for a specific server
+
 def load_data(guild_id, filename):
     path = f"data/{guild_id}/{filename}"
     try:
         with open(path, "r") as file:
-            data = json.load(file)  # use json.load to load the json data from the file
+            data = json.load(file)
         return data
     except FileNotFoundError:
         return {}
 
 
-# Save data for a specific server
 def save_data(guild_id, filename, data):
     directory = f"data/{guild_id}"
-    # Ensure the directory exists
+
     if not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -45,16 +46,13 @@ def save_data(guild_id, filename, data):
         json.dump(data, file)
 
 
-bot = commands.Bot(command_prefix="{}", intents=discord.Intents.all())
-
-
 @bot.event
 async def on_ready():
     await bot.change_presence(
-        status=discord.Status.online,  # You can change this to idle, dnd, etc.
+        status=discord.Status.online,
         activity=discord.Activity(
-            type=discord.ActivityType.playing,  # You can change this to other types
-            name="",  # Unicode representation of the nerd emoji
+            type=discord.ActivityType.playing,
+            name="",
         ),
     )
     print(f"We have logged in as {bot.user}")
@@ -77,7 +75,6 @@ async def on_message(message):
     if random.random() < 0.001:
         await message.add_reaction("💩")
 
-    # 0.004%
     if random.random() < 0.00004:
         emoji = discord.utils.get(message.guild.emojis, name="clown")
         if emoji:
@@ -97,7 +94,6 @@ async def on_message(message):
         for emoji_char in NWORD.split(" "):
             await message.add_reaction(emoji_char)
 
-    # Implement a 10% chance for reacting.
     if random.randint(1, 10) != 1:
         return
 
@@ -119,20 +115,18 @@ async def on_message(message):
             for emoji_char in stinky_data["value"].split(" "):
                 await message.add_reaction(emoji_char)
 
-        # Save the message ID after reacting.
         reacted_messages[message.id] = True
         save_data(guild_id, "reacted_messages.json", reacted_messages)
 
 
 def is_valid_emoji(emoji_str):
-    # Simple check to determine if the string might be a Unicode emoji.
     return emoji_str.encode("unicode-escape").startswith(b"\\U")
 
 
 @bot.tree.command(name="add", description="Add a person or update their emoji")
 async def add(ctx, user: discord.Member, emoji: str):
     guild_id = ctx.guild.id
-    # if ctx.user.id is not in allowed.json
+
     allowed = load_data(guild_id, "allowed.json")
     stinky = load_data(guild_id, "stinky.json")
 
@@ -141,43 +135,39 @@ async def add(ctx, user: discord.Member, emoji: str):
             "You do not have permission to use this command!", ephemeral=True
         )
         return
-    # if user id is the bot
+
     if user.id == bot.user.id:
         await ctx.response.send_message("I can't add myself!", ephemeral=True)
         return
 
-    # Function to convert a word into its emoji counterpart
     def word_to_emoji(word):
         base_emoji_code = ord("🇦")
         emoji_string = ""
-        seen = set()  # To keep track of letters we've seen
+        seen = set()
 
         for char in word:
             if char in seen:
-                return None  # Return None if we see a duplicate letter
+                return None
             seen.add(char)
 
-            # Ensure character is an alphabet and append the corresponding emoji
             if "a" <= char <= "z":
-                emoji_string += chr(base_emoji_code + ord(char) - ord("a")) + " "
+                emoji_string += chr(base_emoji_code +
+                                    ord(char) - ord("a")) + " "
             elif "A" <= char <= "Z":
-                emoji_string += chr(base_emoji_code + ord(char) - ord("A")) + " "
+                emoji_string += chr(base_emoji_code +
+                                    ord(char) - ord("A")) + " "
             else:
-                return (
-                    None  # Return None if the word contains non-alphabetical characters
-                )
+                return None
 
-        return emoji_string.strip()  # Remove trailing space
+        return emoji_string.strip()
 
     if emoji.startswith("<:") or emoji.startswith("<a:") and emoji.endswith(">"):
         emoji_name = emoji.split(":")[1]
         stinky[str(user.id)] = {"type": "custom_emoji", "value": emoji_name}
-    # elif emoji.startswith(":") and emoji.endswith(":"):
-    #     emoji_name = emoji.strip(":")
-    #     stinky[str(user.id)] = {"type": "emoji", "value": emoji_name}
-    elif is_valid_emoji(emoji):  # Check if it's a Unicode emoji.
+
+    elif is_valid_emoji(emoji):
         stinky[str(user.id)] = {"type": "emoji", "value": emoji}
-        emoji_name = emoji  # Ensure emoji_name is set for further processing
+        emoji_name = emoji
     else:
         emoji_name = word_to_emoji(emoji)
         if emoji_name:
@@ -219,7 +209,7 @@ async def remove(ctx, user: discord.Member):
             "You do not have permission to run this command!", ephemeral=True
         )
         return
-    # if user id is the bot
+
     if user.id == bot.user.id:
         await ctx.response.send_message("I can't remove myself!", ephemeral=True)
         return
@@ -238,13 +228,13 @@ async def remove(ctx, user: discord.Member):
                     try:
                         await msg.remove_reaction(emoji_char, bot.user)
                     except discord.HTTPException:
-                        continue  # Skip if the bot hasn't reacted with this emoji
+                        continue
             else:
                 emoji_value = stinky[str(user.id)]["value"]
                 try:
                     await msg.remove_reaction(emoji_value, bot.user)
                 except discord.HTTPException:
-                    continue  # Skip if the bot hasn't reacted with this emoji
+                    continue
         except discord.NotFound:
             continue
 
@@ -258,11 +248,10 @@ async def remove(ctx, user: discord.Member):
     name="clear", description="Remove all reactions from all people on my list"
 )
 async def clear(ctx):
-    guild_id = ctx.guild.id  # Don't forget to get the guild ID
+    guild_id = ctx.guild.id
     allowed = load_data(guild_id, "allowed.json")
     reacted_messages = load_data(guild_id, "reacted_messages.json")
 
-    # Check if the user has administrator permissions or is the owner
     if not ctx.user.guild_permissions.administrator and ctx.user.id not in [
         ctx.guild.owner_id,
         YOUR_ID,
@@ -272,7 +261,6 @@ async def clear(ctx):
         )
         return
 
-    # Send an immediate response
     await ctx.response.send_message("Processing request...")
 
     for message_id in reacted_messages.keys():
@@ -297,18 +285,15 @@ async def clear(ctx):
     reacted_messages.clear()
     save_data(guild_id, "reacted_messages.json", reacted_messages)
 
-    # Send the completion message as a follow-up
     await ctx.followup.send("All reactions from people on the list have been removed.")
 
 
 @bot.tree.command(name="allow", description="these people have a special privilege")
 async def allow(ctx, user: discord.Member):
-    guild_id = ctx.guild.id  # Server's unique ID
+    guild_id = ctx.guild.id
 
-    # Load server-specific allowed data
     allowed = load_data(guild_id, "allowed.json")
 
-    # Only allow server owner (or YOUR_ID) to add people to the allowed list
     if not ctx.user.server_permissions.administrator or ctx.user.id not in [
         ctx.guild.owner_id,
         YOUR_ID,
@@ -333,12 +318,10 @@ async def allow(ctx, user: discord.Member):
     name="disallow", description="Remove special privileges from these people"
 )
 async def disallow(ctx, user: discord.Member):
-    guild_id = ctx.guild.id  # Server's unique ID
+    guild_id = ctx.guild.id
 
-    # Load server-specific allowed data
     allowed = load_data(guild_id, "allowed.json")
 
-    # Only allow server owner (or YOUR_ID) to remove people from the allowed list
     if not ctx.user.server_permissions.administrator or ctx.user.id not in [
         ctx.guild.owner_id,
         YOUR_ID,
@@ -366,7 +349,8 @@ async def _help(ctx):
     embed = discord.Embed(title="Help Command", color=discord.Color.blue())
 
     for command in bot.tree.get_commands():
-        embed.add_field(name=command.name, value=command.description, inline=False)
+        embed.add_field(name=command.name,
+                        value=command.description, inline=False)
 
     await ctx.response.send_message(embed=embed, ephemeral=True)
 
